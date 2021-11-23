f71accfa4264c05e87f890741da0aca93522a601
Shoreniko <sjanj19@freeuni.edu.ge>
2021-11-23 14:23:17 +0400

    2nd assignment initial files

---
+++ Assignment_2_Fun_with_Collections/Word Ladder/Word Ladder/WordLadder.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: WordLadder.cpp
+ * --------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Word Ladder problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    // [TODO: fill with your code]
+    return 0;
+}
