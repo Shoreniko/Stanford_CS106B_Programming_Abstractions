29acd0b8bcc9b4d31992f16d7895150bc58cef3e
Shoreniko <sjanj19@freeuni.edu.ge>
2021-11-23 14:24:34 +0400

    my code/solutions

--- Assignment_2_Fun_with_Collections/Random Writer/Random Writer/RandomWriter.cpp
+++ Assignment_2_Fun_with_Collections/Random Writer/Random Writer/RandomWriter.cpp
@@ -1,17 +1,132 @@
 /*
  * File: RandomWriter.cpp
  * ----------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter project for the Random Writer problem.
- * [TODO: rewrite the documentation]
+ * Name: Shorena Janjghava.
+ * Section: Nikoloz Adeishvili.
+ * This file is the starter project for the Random Writer problem
  */
 
 #include <iostream>
+#include <fstream>
+#include "filelib.h"
+#include "simpio.h"
 #include "console.h"
+#include "vector.h"
+#include "map.h"
+#include "foreach.h"
+#include "set.h"
+#include "random.h"
 using namespace std;
 
+// Function prototype declarations.
+
+void valueIsOutOfRange(int markovOrder);
+void saveFileData(ifstream& input, Vector<char>& fullText);
+bool orderGreaterThanText(Vector<char>& fullText, int markovOrder);
+void analyzeText(Vector<char>& fullText, int markovOrder, Map<string, Vector<char> >& theSequences);
+string findMostFrequentSeed(Map<string, Vector<char> >& theSequences);
+void generateRandomText(string theSeed, Map<string, Vector<char> >& theSequences, int markovOrder);
+
+// Constant variable.
+
+const int RANDOM_TEXT_LENGTH = 2000;
+
+// This code initialises certain elements, reads in information
+// inputed by the user, analyses that information and generates
+// a random text.
+
 int main() {
-    // [TODO: fill with your code]
+	ifstream input;
+	string fileName = promptUserForFile(input, "Enter the source text: ");
+	int markovOrder = getInteger("Enter the Markov order [1-10]: ");
+	valueIsOutOfRange(markovOrder);
+	cout << "Processing file..." << endl;
+	Map<string, Vector<char> > theSequences;
+	Vector<char> fullText;
+	saveFileData(input, fullText);
+	if(orderGreaterThanText(fullText, markovOrder)) return 0; 
+	analyzeText(fullText, markovOrder, theSequences);
+	string theSeed = findMostFrequentSeed(theSequences);
+	generateRandomText(theSeed, theSequences, markovOrder);
     return 0;
+}
+
+// This code deals with the situation when the Markov number entered
+// by the user is out of range.
+
+void valueIsOutOfRange(int markovOrder){
+	while(markovOrder < 1 || markovOrder > 10){
+		cout << "That value is out of range." << endl;
+		markovOrder = getInteger("Enter the Markov order [1-10]: ");
+	}
+}
+
+// The job of this function is to save data from the file indicated by
+// the user.
+
+void saveFileData(ifstream& input, Vector<char>& fullText){
+	char oneChar;
+	while(input.get(oneChar)){
+		fullText.add(oneChar);
+	}
+}
+
+// This function checks whether the Markov number is greater than the
+// file text data or not.
+
+bool orderGreaterThanText(Vector<char>& fullText, int markovOrder){
+	if(fullText.size() < markovOrder){
+		cout << "" << endl;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// The job of this function is to analyze the text by counting special
+// properties. 
+
+void analyzeText(Vector<char>& fullText, int markovOrder, Map<string, Vector<char> >& theSequences){
+	string oneSequence;
+	for(int i = 0; i < fullText.size() - markovOrder; i++){
+		for(int j = 0; j < markovOrder; j++){
+			oneSequence += fullText[i + j];
+		}
+		if(oneSequence.length() == markovOrder){
+			char followingChar = fullText[i + markovOrder];
+			theSequences[oneSequence].add(followingChar);
+			oneSequence = "";
+		}
+	}
+}
+
+// The job of this function is to find the most frequent seed
+// in the file text.
+
+string findMostFrequentSeed(Map<string, Vector<char> >& theSequences){
+	string longestSeed = "";
+	foreach(string seedKey in theSequences){
+		if(theSequences[longestSeed].size() < theSequences[seedKey].size()){
+			longestSeed = seedKey;
+		} 
+	} 
+	return longestSeed;
+}
+
+// The job of this function is to generate the random text. 
+
+void generateRandomText(string theSeed, Map<string, Vector<char> >& theSequences, int markovOrder){
+	string output = theSeed;
+	while(output.length() != RANDOM_TEXT_LENGTH){
+		if(theSequences.containsKey(output.substr(output.length() - markovOrder))){
+			string key = output.substr(output.length() - markovOrder);
+			int maxIndex = theSequences[key].size();
+			char nextRandomChar = theSequences.get(key)[randomInteger(0, maxIndex - 1)];
+			output += nextRandomChar;
+		} else {
+			cout << output << endl;
+			break;
+		}
+	}
+	cout << output << endl;
 }
--- Assignment_2_Fun_with_Collections/Word Ladder/Word Ladder/WordLadder.cpp
+++ Assignment_2_Fun_with_Collections/Word Ladder/Word Ladder/WordLadder.cpp
@@ -1,17 +1,161 @@
 /*
  * File: WordLadder.cpp
  * --------------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
+ * Name: Shorena Janjghava.
+ * Section: Nikoloz Adeishvili.
  * This file is the starter project for the Word Ladder problem.
- * [TODO: rewrite the documentation]
  */
 
 #include <iostream>
 #include "console.h"
+#include "lexicon.h"
+#include "simpio.h"
+#include "strlib.h"
+#include "vector.h"
+#include "queue.h"
+#include "foreach.h"
 using namespace std;
 
+// Function prototype declarations.
+
+int wordLadder();
+bool userWantsToQuit(string startWord);
+void initialPhase(string& startWord, string& endWord, Vector<string>& ladder, Queue<Vector<string> >& queue);
+void findingTheLadder(Queue<Vector<string> >& queue, Lexicon& words, string startWord, string endWord, bool& ladderFound, bool& endWordIsFound, Lexicon& usedWords);
+void finalOutput(Vector<string>& currentLadder, string endWord, bool& ladderFound, Queue<Vector<string> >& queue);
+bool lastIsEndWord(Vector<string>& currentLadder, string endWord);
+void newWordIsFound(string& lastWord, int j, char letter, Lexicon& words, Lexicon& usedWords, Vector<string>& currentLadder, string endWord, Queue<Vector<string> >& queue, bool& endWordIsFound);
+void ladderIsNotFound(bool ladderFound);
+
+// This function generates the word ladder problem.
+
 int main() {
-    // [TODO: fill with your code]
-    return 0;
+	wordLadder();
+	return 0;
 }
+
+// The job of this function is to initialise elements, ask the user for input and 
+// display a word ladder if there is one.
+
+int wordLadder(){
+		while(true){
+		Lexicon words("EnglishWords.dat");
+		Lexicon usedWords;
+		Vector<string> ladder;
+		Queue<Vector<string> > queue;
+		bool ladderFound = false;
+		bool endWordIsFound = false;
+		string startWord = getLine("Enter the starting word (or nothing to quit): "); 
+		if(userWantsToQuit(startWord)) return 0;
+		string endWord = getLine("Enter the ending word: ");
+		initialPhase(startWord, endWord, ladder, queue);
+		findingTheLadder(queue, words, startWord, endWord, ladderFound, endWordIsFound, usedWords);
+		ladderIsNotFound(ladderFound);
+	}
+}
+
+// This code deals with the situation when the user wants to quit the program.
+
+bool userWantsToQuit(string startWord){
+	if(startWord == ""){
+		cout << "Goodbye!" << endl;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// The job of this code is to prepare the program beforehand. 
+
+void initialPhase(string& startWord, string& endWord, Vector<string>& ladder, Queue<Vector<string> >& queue){
+	startWord = toLowerCase(startWord);
+	endWord = toLowerCase(endWord);
+	cout << "Searching..." << endl;
+	ladder.add(startWord);
+	queue.enqueue(ladder);
+}
+
+// The job of this code is to find the word ladder. 
+
+void findingTheLadder(Queue<Vector<string> >& queue, Lexicon& words, string startWord, string endWord, bool& ladderFound, bool& endWordIsFound, Lexicon& usedWords){
+	while(!queue.isEmpty() && words.contains(startWord) && words.contains(endWord)){
+		Vector<string> currentLadder = queue.dequeue();
+		finalOutput(currentLadder, endWord, ladderFound, queue);
+		string lastWord = currentLadder.get(currentLadder.size() - 1);
+		if(!endWordIsFound){ 
+			for(int j = 0; j < lastWord.length(); j++){
+				for(char letter = 'a'; letter <= 'z'; letter++){
+					newWordIsFound(lastWord, j, letter, words, usedWords, currentLadder, endWord, queue, endWordIsFound);
+				}
+			}
+		} 
+	}
+}
+
+// The job of this code is to display a proper message to the user whenever
+// the word ladder is found.
+
+void finalOutput(Vector<string>& currentLadder, string endWord, bool& ladderFound, Queue<Vector<string> >& queue){
+	if(lastIsEndWord(currentLadder, endWord)){
+		cout << "Ladder found: ";
+		foreach (string ladderWord in currentLadder){
+			if(ladderWord == endWord){
+				ladderFound = true;
+				cout << endWord << endl;
+				cout << endl;
+				queue.clear();
+			} else {
+				cout << ladderWord << " -> ";
+			}
+		}
+	}
+}
+
+// The job of this code is to check whether the last word of the
+// ladder is the same as the destination (ending) word.
+
+bool lastIsEndWord(Vector<string>& currentLadder, string endWord){
+	string lastWord = currentLadder.get(currentLadder.size() - 1);
+	if(lastWord == endWord){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+// The job of this code is to find new words and add them to the current
+// ladder. A word will be added to a ladder if it is legal (found in the
+// english words' lexicon) and has not been used in a ladder yet.
+
+void newWordIsFound(string& lastWord, int j, char letter, Lexicon& words, Lexicon& usedWords, Vector<string>& currentLadder, string endWord, Queue<Vector<string> >& queue, bool& endWordIsFound){
+	if(lastWord[j] != letter){
+		string newWord = lastWord;
+		newWord[j] = letter;
+		if(words.contains(newWord) && !usedWords.contains(newWord)){
+			usedWords.add(newWord);
+			Vector<string> newLadder = currentLadder;
+			newLadder += newWord;
+			if(newWord == endWord) endWordIsFound = true; 
+			queue.enqueue(newLadder);
+		}
+	}
+}
+
+// This code outputs a message that tells the user that a word ladder
+// was not found.
+
+void ladderIsNotFound(bool ladderFound){
+	if(!ladderFound){
+		cout << "No word ladder could be found." << endl;
+		cout << endl;
+	}
+}
+
+
+
+
+
+
+
+
